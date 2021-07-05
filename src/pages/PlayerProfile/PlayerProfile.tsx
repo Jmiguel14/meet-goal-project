96ae9f47ff7f0d23b42d8b4d49be0b350503c78f
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2021-07-05 16:42:44 -0500

    Se integra el nombre del usuario al toolbar del perfil

--- src/pages/PlayerProfile/PlayerProfile.tsx
+++ src/pages/PlayerProfile/PlayerProfile.tsx
@@ -10,29 +10,47 @@ import {
   IonCol,
 } from "@ionic/react";
 import { PlayerInfo } from "components/PlayerInfo/PlayerInfo";
+import { useAuth } from "contexts/AuthContext";
+import { useEffect, useState } from "react";
 import "./PlayerProfile.css";
+import firebase from "firebase/app";
+import { getUserDoc } from "firebase/client";
 
-const PlayerProfile: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar className="regresar">
-        <IonButtons slot="start">
-          <IonBackButton
-            defaultHref="/tabs/inicio-jugador"
-            className="icon-back"
-          />
-        </IonButtons>
-        <IonRow className="title">
-          <IonCol size="auto">
-            <IonTitle>UserName</IonTitle>
-          </IonCol>
-        </IonRow>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      <PlayerInfo />
-    </IonContent>
-  </IonPage>
-);
+const PlayerProfile: React.FC = () => {
+  const [datos, setDatos] = useState<
+    firebase.firestore.DocumentData | undefined
+  >();
 
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    let unsubscribe: any;
+    if (currentUser) {
+      unsubscribe = getUserDoc(setDatos);
+    }
+    return () => unsubscribe && unsubscribe();
+  }, [currentUser]);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar className="regresar">
+          <IonButtons slot="start">
+            <IonBackButton
+              defaultHref="/tabs/inicio-jugador"
+              className="icon-back"
+            />
+          </IonButtons>
+          <IonRow className="title">
+            <IonCol size="auto">
+              <IonTitle>{datos?.name}</IonTitle>
+            </IonCol>
+          </IonRow>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <PlayerInfo />
+      </IonContent>
+    </IonPage>
+  );
+};
 export default PlayerProfile;
