c4927f331c2b549c70704fce8c05454a196c1883
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2021-07-13 15:09:23 -0500

    Se agrega el botón de creación de convocatoria

--- src/pages/PlayerDashboard/PlayerDashboard.tsx
+++ src/pages/PlayerDashboard/PlayerDashboard.tsx
@@ -7,26 +7,55 @@ import {
   IonRow,
   IonToolbar,
   IonMenuButton,
+  IonButton,
+  IonIcon,
 } from "@ionic/react";
+import { useAuth } from "contexts/AuthContext";
 import MeetGoal from "icons/MeetGoal";
+import { useEffect, useState } from "react";
 import "./PlayerDashboard.css";
+import firebase from "firebase/app";
+import { getUserDoc } from "firebase/client";
+import { addOutline } from "ionicons/icons";
 
-const PlayerDashboard: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar color="light">
-        <IonButtons slot="start" className="menu-icon">
-          <IonMenuButton />
-        </IonButtons>
-        <IonRow className="ion-justify-content-center">
-          <IonCol size="auto">
-            <MeetGoal width={40} height={40} />
-          </IonCol>
-        </IonRow>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen className="ion-padding"></IonContent>
-  </IonPage>
-);
+const PlayerDashboard: React.FC = () => {
+  const [data, setData] = useState<
+    firebase.firestore.DocumentData | undefined
+  >();
 
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    let unsubscribe: any;
+    if (currentUser) {
+      unsubscribe = getUserDoc(setData);
+    }
+    return () => unsubscribe && unsubscribe();
+  }, [currentUser]);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="light">
+          <IonButtons slot="start" className="menu-icon">
+            <IonMenuButton />
+          </IonButtons>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="auto">
+              <MeetGoal width={40} height={40} />
+            </IonCol>
+          </IonRow>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding">
+        {data?.userType !== "Jugador" ? (
+          <button className="add_calls">
+            <IonIcon icon={addOutline} className="add_calls_icon"></IonIcon>
+          </button>
+        ) : (
+          <div></div>
+        )}
+      </IonContent>
+    </IonPage>
+  );
+};
 export default PlayerDashboard;
