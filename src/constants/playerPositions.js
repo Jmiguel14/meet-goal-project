1113c1a851555d68885ef78ea32e4add214bed14
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-08-26 12:13:33 -0500

    #6 forms data conection fixed and prettier scaned

---
+++ src/constants/playerPositions.js
@@ -0,0 +1,17 @@
+export const PLAYER_POSITIONS = {
+  goalkeeper: "Portero",
+  leftWingBack: "Carrillero izquierdo",
+  rightWingBack: "Carrillero derecho",
+  leftBack: "Lateral izquierdo",
+  rightBack: "Lateral derecho",
+  centreBack: "Defensa central",
+  centralDefensiveMidfielder: "Medio centro defensivo",
+  centralMidfielder: "Medio centro",
+  centralAttakingMidfielder: "Centro medio ofensivo",
+  leftMidfielder: "Medio izquierdo",
+  rightMidfielder: "Medio derecho",
+  outsideLeft: "Externo izquierdo",
+  outsideRight: "Externo derecho",
+  centralStriker: "Delantero central",
+  rightStriker: "Segundo delantero",
+};
