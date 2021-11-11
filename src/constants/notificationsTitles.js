1f444ccc4a60ef4e3929d3fc173ebf2107b28b1b
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2021-11-11 17:18:08 -0500

    typing bugs fixed

--- src/constants/notificationsTitles.js
+++ src/constants/notificationsTitles.js
@@ -1,7 +1,7 @@
 export const NOTIFYTITLES = {
   PLAYERACCEPTED: "Ha sido seleccionado en una convocatoria",
   NEWCALL: "Tu nueva convocatoria se ha creado.",
-  POSTULATION: "Te haz postulado a una convocatoria",
+  POSTULATION: "Te has postulado a una convocatoria",
   PLAYERREJECTED:
     "Desafortunadamente, no has sido seleccionado en una convocatoria",
 };
